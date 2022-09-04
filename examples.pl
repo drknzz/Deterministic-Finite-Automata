c6a91eeaf1f1c59ab0b87eb779d61b2439dd598e
drknzz <kamil.jankowski.x@gmail.com>
2022-09-04 23:01:47 +0200

    v1.0

--- examples.pl
+++ examples.pl
@@ -1,14 +1,14 @@
-% dfa(+TransitionFunction, +Initial, -FinalList)
-% --- DFA has initial state Initial, final states FinalList and
+% dfa(+TransitionFunction, +InitialState, -AcceptStatesSet)
+% --- DFA has initial state InitialState, final states AcceptStatesSet and
 % --- transition function TransitionFunction
 
 % fp(+State, +Letter, +State2)
 % --- exists transition from State to State2 over Letter
 
-% example(+Identificator, +Automaton)
-% --- Automaton has id Identificator
+% example(+AutomatonId, +Automaton)
+% --- Automaton has AutomatonId Identificator
 
-% good automata
+% correct automata
 example(a11, dfa([fp(1,a,1), fp(1,b,2), fp(2,a,2), fp(2,b,1)], 1, [2,1])).
 example(a12, dfa([fp(x,a,y), fp(x,b,x), fp(y,a,x), fp(y,b,x)], x, [x,y])).
 example(a2, dfa([fp(1,a,2), fp(2,b,1), fp(1,b,3), fp(2,a,3), fp(3,b,3), fp(3,a,3)], 1, [1])).
@@ -18,7 +18,7 @@ example(a5, dfa([fp(x,a,y), fp(y,a,z), fp(z,a,zz), fp(zz,a,x)], x, [x])).
 example(a6,  dfa([fp(1,a,1), fp(1,b,2), fp(2,a,2), fp(2,b,1)], 1, [])).
 example(a7,  dfa([fp(1,a,1), fp(1,b,2), fp(2,a,2), fp(2,b,1), fp(3,b,3), fp(3,a,3)], 1, [3])).
 
-% bad automata
+% incorrect automata
 example(b1, dfa([fp(1,a,1), fp(1,a,1)], 1, [])).
 example(b2, dfa([fp(1,a,1), fp(1,a,2)], 1, [])).
 example(b3, dfa([fp(1,a,2)], 1, [])).
